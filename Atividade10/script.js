d4655df1ab3ea12a2b7dcb51c3d92300979907a7
Mariana de Brito Morosini <78965428+MarianaMorosini@users.noreply.github.com>
2022-11-09 21:29:15 -0300

    Add files via upload

---
+++ Atividade10/script.js
@@ -0,0 +1,18 @@
+var aluno1 = new Object();
+aluno1.RA = 1023456789;
+aluno1.nome = "Mariana de Brito Morosini";
+
+var aluno2 = {};
+aluno2.RA = 1234567890;
+aluno2.nome = "Mario";
+
+var aluno3 = {
+    RA : 2345678901,
+    nome : "Carla"
+};
+
+console.log(aluno1);
+console.log(aluno2);
+console.log(aluno3);
+
+alert("Olhar os objetos no console");
